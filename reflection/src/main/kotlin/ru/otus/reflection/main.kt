e6ea7597d3a5dc05ca72bd4e763c9a00a8d23bf2
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-08-12 13:51:25 +0200

    Simple reflection

--- reflection/src/main/kotlin/ru/otus/reflection/main.kt
+++ reflection/src/main/kotlin/ru/otus/reflection/main.kt
@@ -3,4 +3,12 @@ package ru.otus.reflection
 fun main() {
     val somethingElse = SomethingElse(100)
     println(somethingElse)
+    reflect()
+}
+
+fun reflect() {
+    val something = SomethingElse(100)
+    val methods = SomethingElse::class.java.methods
+    val someFun = methods.find { it.name == "someFun" }
+    someFun?.invoke(something)
 }
