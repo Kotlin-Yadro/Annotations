90ebd9770af129f65350619624c96547ac630c9f
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-08-12 12:03:25 +0200

    Validation

--- processor/src/main/kotlin/ru/otus/processor/PrintableClassProcessor.kt
+++ processor/src/main/kotlin/ru/otus/processor/PrintableClassProcessor.kt
@@ -2,6 +2,8 @@ package ru.otus.processor
 
 import com.google.devtools.ksp.processing.*
 import com.google.devtools.ksp.symbol.KSAnnotated
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.validate
 import ru.otus.annotation.PrintableClass
 
 class PrintableClassProcessor(
@@ -11,10 +13,16 @@ class PrintableClassProcessor(
     override fun process(resolver: Resolver): List<KSAnnotated> {
         val toSearchFor = requireNotNull(PrintableClass::class.qualifiedName)
         val symbols: Sequence<KSAnnotated> = resolver.getSymbolsWithAnnotation(toSearchFor)
-        symbols.forEach {
-            logger.warn("Found symbols: $it")
+
+        val validated = symbols
+            .filterIsInstance<KSClassDeclaration>()
+            .filter { it.validate() }
+            .toList()
+
+        validated.forEach {
+            logger.warn("Found classes: $it")
         }
-        return emptyList()
+        return symbols.toList() - validated.toSet()
     }
 }
 
